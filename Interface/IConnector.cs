3b24898c635d80f36093988aa936591b36bd12d5
Avngarde <pivotguy888@gmail.com>
2022-03-13 16:14:17 +0100

    Created IConnector interface

---
+++ Interface/IConnector.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Privas.Interface
+{
+    public interface IConnector
+    {
+        private MongoClient Client => new("MongoDBPasses");
+        IMongoDatabase Database => Client.GetDatabase("Privas");
+        IMongoCollection<BsonDocument> Collection { get; set; }
+        BsonDocument GetAll();
+        BsonDocument Get();
+        void Add();
+        UpdateResult Update();
+        DeleteResult Delete();
+    }
+}
