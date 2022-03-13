176aeaaeb194c2cab7654c5628e01640f5a9331b
Avngarde <pivotguy888@gmail.com>
2022-03-13 23:59:29 +0100

    Modified IConnector, Created ChatroomConnector

--- Interface/IConnector.cs
+++ Interface/IConnector.cs
@@ -5,9 +5,8 @@ namespace Privas.Interface
 {
     public interface IConnector
     {
-        private MongoClient Client => new("mongodb+srv://500PlusClient:SecretsOfVatican1978@cluster0.1kawz.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        IMongoDatabase Database => Client.GetDatabase("Privas");
-        IMongoCollection<BsonDocument> Collection { get; set; }
+        private static MongoClient Client => new("mongodb+srv://500PlusClient:SecretsOfVatican1978@cluster0.1kawz.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        static IMongoDatabase Database => Client.GetDatabase("Privas");
         BsonDocument GetAll();
         BsonDocument Get();
         void Add();
