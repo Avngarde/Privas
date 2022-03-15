3e4c44b042ab993378687aa5c6113bd1279df72e
Avngarde <pivotguy888@gmail.com>
2022-03-15 17:28:14 +0100

    Got rid of CS8618 from Chatroom

--- Data/Chatroom.cs
+++ Data/Chatroom.cs
@@ -2,9 +2,9 @@ namespace Privas.Data
 {
     public class Chatroom
     {
-        public string Name { get; set; }
-        public string UniqueCode { get; set; }
-        public string AccessPassword { get; set; }
+        public string? Name { get; set; }
+        public string? UniqueCode { get; set; }
+        public string? AccessPassword { get; set; }
         
     }
 }
