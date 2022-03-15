3eea33b5bd6eb7bb7448f541f30f7bd6cf2b5c93
Avngarde <pivotguy888@gmail.com>
2022-03-15 15:37:10 +0100

    Modified Chatroom model

--- Data/Chatroom.cs
+++ Data/Chatroom.cs
@@ -2,6 +2,7 @@ namespace Privas.Data
 {
     public class Chatroom
     {
+        public string Name { get; set; }
         public string UniqueCode { get; set; }
         public string AccessPassword { get; set; }
         
