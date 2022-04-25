b298b92850099088f89c9a5b03a0c64ba225f0f8
Avngarde <pivotguy888@gmail.com>
2022-04-26 00:27:05 +0200

    Created MessageConnector, Modified Message model

---
+++ Connectors/MessageConnector.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Privas.Interface;
+
+namespace Privas.Connectors
+{
+    public class MessageConnector : IConnector
+    {
+        public IMongoCollection<BsonDocument> Collection = IConnector.Database.GetCollection<BsonDocument>("messages");
+
+        public async Task Add(BsonDocument newMessage)
+        {
+            await Collection.InsertOneAsync(newMessage);
+        }
+
+        public async Task<DeleteResult> Delete(string messageId)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("MessageId", messageId);
+            return await Collection.DeleteOneAsync(filter);
+        }
+
+        public async Task<BsonDocument> Get(string messageId)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("MessageId", messageId);
+            var searchResult = await Collection.FindAsync(filter);
+            return await searchResult.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<BsonDocument>> GetAll()
+        {
+            return await Collection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task<ReplaceOneResult> Update(string messageId, BsonDocument message)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("MessageId", messageId);
+            return await Collection.ReplaceOneAsync(filter, message);
+        }
+    }
+}
--- Data/Membership.cs
+++ Data/Membership.cs
@@ -2,6 +2,7 @@ namespace Privas.Data
 {
     public class Membership
     {
+        public string? MessageId { get; set; }
         public string? ChatId { get; set; }   
         public string? UserId { get; set; } 
         public string? UserName { get; set; }           
