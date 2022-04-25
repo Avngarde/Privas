920b111d36ea6dd890608e04229dfd9451840c86
Avngarde <pivotguy888@gmail.com>
2022-04-25 22:17:17 +0200

    Created Message model

---
+++ Data/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Privas.Data
+{
+    public class Message
+    {
+        public string? ChatId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserId { get; set; }
+        public string? Text { get; set; }
+    }
+}
