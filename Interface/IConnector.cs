fe0dab065a3769b727b07623b2b483fa645f3269
Avngarde <pivotguy888@gmail.com>
2023-04-02 20:45:53 +0200

    Minor changes to the css, changed connector password

--- Interface/IConnector.cs
+++ Interface/IConnector.cs
@@ -5,7 +5,7 @@ namespace Privas.Interface
 {
     public interface IConnector
     {
-        private static MongoClient Client => new("mongodb+srv://500PlusClient:SecretsOfVatican1978@cluster0.1kawz.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        private static MongoClient Client => new("mongodb+srv://500PlusClient:Shaman2002$@cluster0.1kawz.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
         static IMongoDatabase Database => Client.GetDatabase("Privas");
         Task<List<MongoDB.Bson.BsonDocument>> GetAll();
         Task<BsonDocument> Get(string id);
